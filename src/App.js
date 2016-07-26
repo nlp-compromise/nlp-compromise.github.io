cfdff77224532fd28da2227d2278832fb2bfc9af
Spencer Kelly <spencermountain@gmail.com>
2016-07-26 13:44:04 -0400

    starting resized textarea

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Input from './input';
+
+class Expo extends Component {
+  render() {
+    return (
+      <div >
+        <Input text='the blue dog jumps'/>
+      </div>
+      );
+  }
+}
+
+export default Expo;
