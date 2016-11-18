3d8fa08ae01a46c1aee485124baeeac08f7f919c
spencermountain <spencermountain@gmail.com>
2016-11-18 11:21:01 -0500

    yolo

--- src/index.js
+++ src/index.js
@@ -6,14 +6,31 @@ import Firebase from './firebase';
 import Bottom from './bottom';
 import debounce from './debounce';
 import Source from './source';
+import Code from './code';
 import styler from 'react-styling/flat';
 import nlp from 'nlp_compromise';
 
-const style = styler`
+const style = styler `
+@font-face {
+  font-family: 'Slabo 27px';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Slabo 27px'), local('Slabo27px-Regular'), url(https://fonts.gstatic.com/s/slabo27px/v3/LfR9_S_HMdQ73mwIHBRxoSEAvth_LlrfE80CYdSH47w.woff2) format('woff2');
+  unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Slabo 27px';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Slabo 27px'), local('Slabo27px-Regular'), url(https://fonts.gstatic.com/s/slabo27px/v3/PuwvqkdbcqU-fCZ9Ed-b7fk_vArhqVIZ0nv9q090hN8.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+}
 container
   width: 80%
   margin: 9%
   padding: 8
+  font-family: 'Slabo 27px', serif;
 textarea
   color: grey
   width:100%
@@ -59,23 +76,49 @@ class Main extends React.Component {
     console.timeEnd('parse');
   }
   render() {
-    let {state, css} = this;
-    return (
-      <div style={css.container}>
-        <Source src={state.src} cmp={this}/>
-        <Textarea
-      value={state.text}
-      maxRows={8}
-      style={css.textarea}
-      onChange={this.onType}/>
-      <Bottom result={state.result} cmp={this}/>
-    </div>
-      );
+    let {
+      state,
+      css
+    } = this;
+    return ( < div style = {
+        css.container
+      } >
+      <
+      Source src = {
+        state.src
+      }
+      cmp = {
+        this
+      }
+      /> <
+      Textarea value = {
+        state.text
+      }
+      maxRows = {
+        8
+      }
+      style = {
+        css.textarea
+      }
+      onChange = {
+        this.onType
+      }
+      /> <
+      Bottom result = {
+        state.result
+      }
+      cmp = {
+        this
+      }
+      /> < /
+      div >
+    );
   }
 }
 Main = Radium(Main);
 
-ReactDOM.render(
-  <Main />,
+ReactDOM.render( < Main / > ,
   document.getElementById('root')
+);
+Id('root')
 );
