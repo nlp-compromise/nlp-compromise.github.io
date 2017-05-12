72624be7c81659c71c94e6b9d241e016071f0113
spencermountain <spencermountain@gmail.com>
2017-05-12 16:39:21 -0400

    0.0.4

--- src/shared/nlp.js
+++ src/shared/nlp.js
@@ -1,8 +1,7 @@
-// import nlp from 'compromise';
-// import nlp from 'compromise';
-// import docs from '../../node_modules/compromise/docs/api';
-import nlp from '/home/spencer/nlp/nlp_compromise/src/index';
-import docs from '/home/spencer/nlp/nlp_compromise/docs/api';
+import nlp from 'compromise';
+import docs from '../../node_modules/compromise/docs/api';
+// import nlp from '/home/spencer/nlp/nlp_compromise/src/index';
+// import docs from '/home/spencer/nlp/nlp_compromise/docs/api';
 module.exports = {
   nlp: nlp,
   version: nlp.version,
